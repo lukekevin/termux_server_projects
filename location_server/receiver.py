904d1a8f9c0f4b5dff5cbea339ba337304e29782
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2024-04-23 19:59:43 +0530

    Update receiver.py

--- location_server/receiver.py
+++ location_server/receiver.py
@@ -5,7 +5,7 @@ import time
 def get_location():
     # Use ipconfig or any other method to get Device B's IP address
     # Replace 'DEVICE_B_IP' with the actual IP address of Device B
-    device_b_ip = '192.168.1.106' #replace by any local ip address
+    device_b_ip = '192.168.1.106'
     ping_response = subprocess.run(['ping', '-c', '1', device_b_ip], capture_output=True)
     if ping_response.returncode == 0:
         # Ping successful, send a request to Device B for location
@@ -13,10 +13,11 @@ def get_location():
         if response.status_code == 200:
             location_data = response.json()
             print("Location:", location_data)
+            print("The payload is in good health")
         else:
-            print("Failed to get location from Device B")
+            print("The payload experiencing problems")
     else:
-        print("Device B is not reachable")
+        print("Transmission stopped")
 
 while True:
     get_location()
