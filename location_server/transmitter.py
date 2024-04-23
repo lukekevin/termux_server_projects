0f329171a257e2f7023ec0bd13b898f9c207fab3
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2024-04-23 20:15:32 +0530

    Update transmitter.py

--- location_server/transmitter.py
+++ location_server/transmitter.py
@@ -1,7 +1,17 @@
 from flask import Flask, jsonify
 import requests
+import subprocess
 
 app = Flask(__name__)
+
+def get_current_time():
+    try:
+        # Execute the 'date' command using subprocess and capture its output
+        time_output = subprocess.check_output(['date']).decode().strip()
+        return time_output
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+        return None
 
 def get_location_from_ip():
     # Using a free IP-based geolocation service
@@ -18,12 +28,16 @@ def get_location_from_ip():
 
 @app.route('/get_location')
 def get_location():
-    # Get location from IP-based geolocation service
+    # Get location from the geolocation-db.com API
     location_data = get_location_from_ip()
     if location_data:
+        # Get current time
+        current_time = get_current_time()
+        if current_time:
+            location_data['current_time'] = current_time
         return jsonify(location_data)
     else:
         return jsonify({'error': 'Failed to retrieve location'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4567)
+    app.run(host='0.0.0.0', port=4567) 
